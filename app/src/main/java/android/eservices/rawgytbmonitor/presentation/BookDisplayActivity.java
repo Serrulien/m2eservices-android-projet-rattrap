ce606096e867f6e60770c50fe462f953947424bd
Julien Serrurier <contact@julienserrurier.fr>
2020-06-30 14:23:38 +0200

    Création d'un style pour coller au thème du site web, ajout des icones pour changer de fragment

--- app/src/main/java/android/eservices/rawgytbmonitor/presentation/BookDisplayActivity.java
+++ app/src/main/java/android/eservices/rawgytbmonitor/presentation/BookDisplayActivity.java
@@ -10,9 +10,18 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class BookDisplayActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
+    private TabLayout tabLayout;
+
+    private int[] tabIcons = {
+            R.drawable.ic_subscriptions_24dp,
+            R.drawable.ic_apps_24dp,
+            R.drawable.ic_search_24dp,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,32 +32,34 @@ public class BookDisplayActivity extends AppCompatActivity {
 
     private void setupViewPagerAndTabs() {
         viewPager = findViewById(R.id.tab_viewpager);
+        tabLayout = findViewById(R.id.tablayout);
 
         final SearchFragment searchFragment = SearchFragment.newInstance();
         final FavoriteFragment fragmentTwo = FavoriteFragment.newInstance();
+        final FavoriteFragment fragmentThree = FavoriteFragment.newInstance();
 
         viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
             @Override
             public Fragment getItem(int position) {
                 if (position == 0) {
                     return searchFragment;
+                } else if (position == 1) {
+                    return fragmentTwo;
                 }
-                return fragmentTwo;
-            }
-
-            @Override
-            public CharSequence getPageTitle(int position) {
-                if (position == 0) {
-                    return SearchFragment.TAB_NAME;
-                }
-                return FavoriteFragment.TAB_NAME;
+                return fragmentThree;
             }
 
             @Override
             public int getCount() {
-                return 2;
+                return 3;
             }
         });
+
+        tabLayout.setupWithViewPager(viewPager);
+        for(int i = 0; i < tabIcons.length; i++) {
+            TabLayout.Tab tab = tabLayout.getTabAt(i);
+            tab.setIcon(tabIcons[i]);
+        }
     }
 
 
